898516a800f95aa8f4f8fe24959d7cbc6061bd3b
dsttamal <dsttamal@gmail.com>
2024-03-20 10:07:39 +0600

    first commit

---
+++ main.py
@@ -0,0 +1,14 @@
+import subprocess
+
+# Define the FFmpeg command as a string
+ffmpeg_command = """ffmpeg -f dshow -i video="@device_pnp_\\\\?\\usb#vid_01da&pid_5875&mi_00#6&770fab3&0&0000#{65e8773d-8f56-11d0-a3b9-00a0c9223196}\\global" -c:v libx264 -preset medium -f flv rtmp://server.dsttamal.me:1935/stream1"""
+
+# Use subprocess to run the command
+# It's important to use shell=True on Windows if the command is a string
+process = subprocess.run(ffmpeg_command, shell=True)
+
+# Check if the process was successful
+if process.returncode == 0:
+    print("FFmpeg command executed successfully.")
+else:
+    print("FFmpeg command failed with return code:", process.returncode)
